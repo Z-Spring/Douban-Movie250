b3863159bcd79d098ace3dda845ba47da84d7b85
murphy <murphyqq1@gmail.com>
2022-06-28 14:02:02 +0800

    fix: add cookie to header

--- movie/hi.go
+++ movie/hi.go
@@ -21,6 +21,8 @@ func GetMovieBody(start int) []byte {
 
 	}
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36")
+	// 此cookie是登录豆瓣后从浏览器获得，因为有时间限制，需要勤换
+	req.Header.Add("cookie", "ll=\"108288\"; bid=tfmu0VNtswM; douban-fav-remind=1; ct=y; viewed=\"26979890_30357170_30397714_27663285_30389976_25782902_19952400_35410754_35668581_30395692\"; push_noty_num=0; push_doumail_num=0; ap_v=0,6.0; dbcl2=\"156810236:67LMVXkh0Sw\"; ck=1CSY")
 
 	res, err := client.Do(req)
 	if err != nil {
